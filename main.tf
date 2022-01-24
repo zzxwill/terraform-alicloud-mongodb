bc1c9504edb40ac783db8b39ffc0943b5b3b991c
Zheng Xi Zhou <zzxwill@gmail.com>
2022-01-24 15:19:52 +0800

    remove cms

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 locals {
   this_instance_id      = var.existing_instance_id != "" ? var.existing_instance_id : concat(alicloud_mongodb_instance.this.*.id, [""])[0]
   create_more_resources = var.existing_instance_id != "" || var.create ? true : false
-  project               = "acs_mongodb"
 }
 
 resource "alicloud_mongodb_instance" "this" {
@@ -21,100 +20,4 @@ resource "alicloud_mongodb_instance" "this" {
   backup_period        = var.backup_period
   backup_time          = var.backup_time
   tags                 = var.tags
-}
-
-resource "alicloud_cms_alarm" "cpu_utilization" {
-  count   = local.create_more_resources && var.enable_alarm_rule ? 1 : 0
-  enabled = var.enable_alarm_rule
-  name    = var.alarm_rule_name
-  project = local.project
-  metric  = "CPUUtilization"
-  dimensions = {
-    instanceId = local.this_instance_id
-    device     = "/dev/vda1,/dev/vdb1"
-  }
-  statistics         = var.alarm_rule_statistics
-  period             = var.alarm_rule_period
-  operator           = var.alarm_rule_operator
-  threshold          = var.alarm_rule_threshold
-  triggered_count    = var.alarm_rule_triggered_count
-  contact_groups     = var.alarm_rule_contact_groups
-  silence_time       = var.alarm_rule_silence_time
-  effective_interval = var.alarm_rule_effective_interval
-}
-resource "alicloud_cms_alarm" "memory_utilization" {
-  count   = local.create_more_resources && var.enable_alarm_rule ? 1 : 0
-  enabled = var.enable_alarm_rule
-  name    = var.alarm_rule_name
-  project = local.project
-  metric  = "MemoryUtilization"
-  dimensions = {
-    instanceId = local.this_instance_id
-    device     = "/dev/vda1,/dev/vdb1"
-  }
-  statistics         = var.alarm_rule_statistics
-  period             = var.alarm_rule_period
-  operator           = var.alarm_rule_operator
-  threshold          = var.alarm_rule_threshold
-  triggered_count    = var.alarm_rule_triggered_count
-  contact_groups     = var.alarm_rule_contact_groups
-  silence_time       = var.alarm_rule_silence_time
-  effective_interval = var.alarm_rule_effective_interval
-}
-resource "alicloud_cms_alarm" "disk_utilization" {
-  count   = local.create_more_resources && var.enable_alarm_rule ? 1 : 0
-  enabled = var.enable_alarm_rule
-  name    = var.alarm_rule_name
-  project = local.project
-  metric  = "DiskUtilization"
-  dimensions = {
-    instanceId = local.this_instance_id
-    device     = "/dev/vda1,/dev/vdb1"
-  }
-  statistics         = var.alarm_rule_statistics
-  period             = var.alarm_rule_period
-  operator           = var.alarm_rule_operator
-  threshold          = var.alarm_rule_threshold
-  triggered_count    = var.alarm_rule_triggered_count
-  contact_groups     = var.alarm_rule_contact_groups
-  silence_time       = var.alarm_rule_silence_time
-  effective_interval = var.alarm_rule_effective_interval
-}
-resource "alicloud_cms_alarm" "intranet_in" {
-  count   = local.create_more_resources && var.enable_alarm_rule ? 1 : 0
-  enabled = var.enable_alarm_rule
-  name    = var.alarm_rule_name
-  project = local.project
-  metric  = "IntranetIn"
-  dimensions = {
-    instanceId = local.this_instance_id
-    device     = "/dev/vda1,/dev/vdb1"
-  }
-  statistics         = var.alarm_rule_statistics
-  period             = var.alarm_rule_period
-  operator           = var.alarm_rule_operator
-  threshold          = var.alarm_rule_threshold
-  triggered_count    = var.alarm_rule_triggered_count
-  contact_groups     = var.alarm_rule_contact_groups
-  silence_time       = var.alarm_rule_silence_time
-  effective_interval = var.alarm_rule_effective_interval
-}
-resource "alicloud_cms_alarm" "intranet_out" {
-  count   = local.create_more_resources && var.enable_alarm_rule ? 1 : 0
-  enabled = var.enable_alarm_rule
-  name    = var.alarm_rule_name
-  project = local.project
-  metric  = "IntranetOut"
-  dimensions = {
-    instanceId = local.this_instance_id
-    device     = "/dev/vda1,/dev/vdb1"
-  }
-  statistics         = var.alarm_rule_statistics
-  period             = var.alarm_rule_period
-  operator           = var.alarm_rule_operator
-  threshold          = var.alarm_rule_threshold
-  triggered_count    = var.alarm_rule_triggered_count
-  contact_groups     = var.alarm_rule_contact_groups
-  silence_time       = var.alarm_rule_silence_time
-  effective_interval = var.alarm_rule_effective_interval
 }
