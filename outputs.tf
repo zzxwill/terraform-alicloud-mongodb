5621d3502b2c6b5c078c119190b2e25823f46acc
Zheng Xi Zhou <zzxwill@gmail.com>
2022-01-24 15:34:49 +0800

    clean outputs

--- outputs.tf
+++ outputs.tf
@@ -17,73 +17,7 @@ output "this_mongodb_instance_id" {
   value       = local.this_instance_id
 }
 
-output "this_mongodb_instance_engine_version" {
-  description = "The engine version of the MongoDB instance. "
-  value       = local.this_mongodb_instance_engine_version
-}
-
-output "this_mongodb_instance_charge_type" {
-  description = "The billing method of the instance. "
-  value       = local.this_mongodb_instance_charge_type
-}
-
-output "this_mongodb_instance_period" {
-  description = "The duration that you will buy MongoDB instance. "
-  value       = concat(alicloud_mongodb_instance.this.*.period, [""])[0]
-}
-
-output "this_mongodb_instance_zone_id" {
-  description = "The Zone to launch the MongoDB instance. "
-  value       = concat(alicloud_mongodb_instance.this.*.zone_id, [""])[0]
-}
-
-output "this_mongodb_instance_vswitch_id" {
-  description = "The virtual switch ID to launch MongoDB instances in one VPC. "
-  value       = concat(alicloud_mongodb_instance.this.*.vswitch_id, [""])[0]
-}
-
-output "this_mongodb_instance_tags" {
-  description = "A mapping of tags to assign to the MongoDB instance resource. "
-  value       = alicloud_mongodb_instance.this.*.tags
-}
-
-output "this_mongodb_instance_storage" {
-  description = "User-defined DB instance storage space. "
-  value       = local.this_db_instance_storage
-}
-
-output "this_mongodb_instance_class" {
-  description = "The class of MongoDB instance"
-  value       = local.this_db_instance_class
-}
-
-output "this_mongodb_instance_storage_engine" {
-  description = "The storage engine used by the MongoDB instance. "
-  value       = concat(alicloud_mongodb_instance.this.*.storage_engine, [""])[0]
-}
-
 output "this_mongodb_instance_name" {
   description = "The name of MongoDB instance. "
   value       = local.this_db_instance_name
 }
-
-output "this_mongodb_instance_security_ip_list" {
-  description = "IP whitelist of MongoDB instances. "
-  value       = alicloud_mongodb_instance.this.*.security_ip_list
-}
-
-output "this_mongodb_instance_replication_factor" {
-  description = "Number of replica set nodes. "
-  value       = concat(alicloud_mongodb_instance.this.*.replication_factor, [""])[0]
-}
-
-output "this_mongodb_instance_backup_period" {
-  description = "The MongoDB instance backup period. "
-  value       = alicloud_mongodb_instance.this.*.backup_period
-}
-
-output "this_mongodb_instance_backup_time" {
-  description = "The MongoDB instance backup time. "
-  value       = concat(alicloud_mongodb_instance.this.*.backup_time, [""])[0]
-}
-
